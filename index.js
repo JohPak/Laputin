1066cc033f28032c2fa46741f341a1c3eebea975
JohannaP <55647359+JohPak@users.noreply.github.com>
2021-08-12 23:58:33 +0300

    eka versio

---
+++ index.js
@@ -0,0 +1,101 @@
+//käynnistä "npm start". Jos haluaa suorittaa scriptissä olevan komennon, sitten "npm run start"
+//käynnistä nodemonilla: npm run start-dev
+
+const fs = require('fs');
+const cheerio = require('cheerio');
+const got = require('got');
+const http = require('http');
+const express = require('express');
+
+let app = express();
+
+
+// console.log(url);
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.json()) // To parse the incoming requests with JSON payloads
+
+// SERVE STATIC FILES Now, you can load the files that are in the public directory
+// app.use(express.static('public')) 
+app.use(express.static(__dirname)); //NODEN käynnistyssijainti - saattaa muuttua jos appin siirtää jonnekin
+
+let url= "";
+let ean = "ean-koodi";
+let tuote = "tuote";
+let hinta = "hinta";
+let kuvaus = "kuvaus";
+let kuva = "https://www.minimani.fi/media/catalog/product/placeholder/default/minimaniph.png";
+
+
+
+let haeSisalto = function() {
+console.log("siirrytty haeSisaltoon");
+//HAETAAN SISÄLTÖ
+got(url).then(response => {
+        console.log("siirrytty Gotiin");
+        const $ = cheerio.load(response.body);
+        // console.log($('a')[0]);
+        
+        // $('h1').each((i, tulos) => {
+            //     const sisalto = tulos;
+            //     console.log(sisalto);
+            //   });
+            
+            ean = $('div[itemprop="sku"]').text();
+            tuote = $('.base').text();
+            hinta = $('meta[itemprop="price"]').attr("content");
+            // kuvaus = $('.value p').text();
+            kuvaus = $('div.value:nth-child(1)').text();
+            kuva = "https://www.minimani.fi/media/catalog/product/" + ean.charAt(0) + "/" + ean.charAt(1) + "/" + ean + ".jpg";
+            
+            //korvataan hinnan piste pilkulla
+            hinta = hinta.replace(".", ",");
+            
+        }).catch(err => {
+            console.log(err);
+        });
+} // END HAESISALTO
+
+
+// hae html
+app.get('/', function (req, res, next) {
+    console.log("Siirrytty app.get /-funktioon");
+    if (url.includes("www.minimani.fi/")) {
+        
+        
+        // res.sendFile(__dirname+'/views/index.html');
+        // nodessa ei voi käpistellä html-tiedostoja suoraan, joten muutetaan se stringiksi
+        let html = fs.readFileSync("./views/index.html").toString("utf-8");
+        
+        html = html.replace("https://www.minimani.fi/media/catalog/product/placeholder/default/minimaniph.png", kuva);
+        html = html.replace("{tuote}", tuote);
+        html = html.replace("{ean}", ean);
+        html = html.replace("{hinta}", hinta + " €");
+        html = html.replace("{kuvaus}", kuvaus);
+        html = html.replace("{url}", url);
+        
+        //lähetetään valmis sisältö
+        res.send(html);
+    }
+    else {
+        console.log("Url puuttuu!");
+        let html = fs.readFileSync("./views/index.html").toString("utf-8");
+        html = html.replace("{url}", "Anna tuotteelle osoite");
+        res.send(html);
+    }
+  });
+
+app.post('/hae', function (req, res, next) {
+    url = req.body.urlinput;   
+    console.log("Saatu url: " + url);
+    haeSisalto();
+    res.redirect("/");
+})
+
+
+app.listen(8080);
+
+
+
+
+
